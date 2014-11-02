fea8872154c8c79606fc1e11cc7914a1bc2755f4
mimosafa <mimosafa@gmail.com>
2014-11-03 06:32:42 +0900

    First commit

---
+++ plugin.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+Plugin Name: MMSF Degug Var
+Plugin URI: 
+Description: 
+Author: mmsf
+Version: 0.0
+Author URI: http://mimosafa.me/
+*/
+
+class mmsf_var_dump {
+
+	private $vars;
+
+	private $hook = '';
+
+	private function __construct( $var, $hook ) {
+		$this -> vars = $var;
+
+		if ( !$hook ) {
+			if ( is_admin() ) {
+				$this -> hook = 'admin_notices';
+			}
+		}
+
+		if ( $this -> hook ) {
+			add_action( $this -> hook, [ $this, 'var_dump' ] );
+		}
+	}
+
+	private function var_dump() {
+		echo '<pre>';
+		var_dump( $this -> vars );
+		echo '</pre>';
+	}
+
+	public static function vars( $var, $hook = '' ) {
+		$cl = new self( $var );
+	}
+
+}
+
+/**
+ *
+ */
+if ( !function_exists( '_var_dump' ) ) {
+	function _var_dump( $var ) {
+		mmsf_var_dump::vars( $var );
+	}
+}
